3f512ecb5ae1ffb246625ef3b4f350b0a7427fea
LEVENT-BAL <119902197+LEVENT-BAL@users.noreply.github.com>
2025-06-02 17:53:02 +0300

    İlk commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+using basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers;
+
+public class CourseController : Controller
+{
+    public IActionResult Index()
+    {
+        var kurs = new Course();
+        kurs.Id = 1;
+        kurs.Title = "Aspnet core kursu";
+        kurs.Description = "Güzel bir kurs";
+
+        return View(kurs);
+    }
+
+    public IActionResult List()
+    {
+
+        var kurslar = new List<Course>() 
+        {
+            new Course() {Id=1, Title="aspnet kursu", Description="güzel bir kurs"},
+            new Course() {Id=2, Title="php kursu", Description="güzel bir kurs"},
+            new Course() {Id=3, Title="django kursu", Description="güzel bir kurs"}
+
+        };
+
+        return View("CourseList", kurslar);
+    }
+    
+    
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View ();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    
+    
+}
